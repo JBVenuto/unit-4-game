34ee3f017cfb310cdf6c9efc4f21baa38660ccbd
Joseph Venuto <jbvenuto@gmail.com>
2018-07-20 17:31:48 -0400

    Added html and comment outlines

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//Makes random numbers for the target number and the four crystal numbers
+
+//Variable to track the user's current score
+
+//Checks to see what button was clicked and add that button's value to the user's score
+
+//Check to see if the user's score is equal to or over the target number
+
+//Make a new set of random numbers
