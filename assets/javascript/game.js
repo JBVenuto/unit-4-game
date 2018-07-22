58eb085cb1d3f0065bdfa2c29d8c6091c3d39b37
Joseph Venuto <jbvenuto@gmail.com>
2018-07-22 14:43:51 -0400

    put in a background image and added instructions for the user

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,9 @@ var red;
 var yellow;
 //User's score
 var userScore;
+//Wins and loses
+var wins = 0;
+var loses = 0;
 
 //Function to start the game
 function startGame () {
@@ -20,19 +23,24 @@ function startGame () {
     //Set the user's score to zero at the start of the game
     userScore = 0;
 
+
 }
 //Run start game function
 startGame();
 
 //Print the values of the jewels in the console for testing
+console.log(target);
 console.log(green);
 console.log(purple);
 console.log(red);
 console.log(yellow);
 
 $(document).ready(function() {
-    //Checks to see what button was clicked and add that button's value to the user's score
+    //Display the user's score and target number
+    // document.getElementById("endNumber").innerHTML = "The target score is" + target ".";
+    // document.getElementById("userSum").innerHTML = userScore;
 
+    //Checks to see what button was clicked and add that button's value to the user's score
     $("#greenJewel").on("click", function() {
         userScore += green;
         console.log("User scroe: " + userScore);
@@ -40,20 +48,30 @@ $(document).ready(function() {
     $("#purpleJewel").on("click", function() {
         userScore += purple;
         console.log("User scroe: " + userScore);
-
     })
     $("#redJewel").on("click", function() {
         userScore += red;
         console.log("User scroe: " + userScore);
-
     })
     $("#yellowJewel").on("click", function() {
         userScore += yellow;
         console.log("User scroe: " + userScore);
-
     })
 
+    //When a crystal is clicked check to see if the user score is at the target score or above
+    $("#crystals").on("click", function () {
+    //Check to see if the user's score is equal to the target number
+    if (userScore === target) {
+        alert("Congratulations! You reached the target score!");
+        wins++;
+        startGame();
+    }
+
+    //Check to see if the user goes over the target score
+    if(userScore > target) {
+        alert("Oh no! You passed the target score! Give it another try!");
+        loses++;
+        startGame();
+    }
+    })
 })
-
-//Check to see if the user's score is equal to or over the target number
-
